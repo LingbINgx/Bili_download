37b323076c0452cf1d69d038494e31f8d155fce4
LingbINgx <146912904+LingbINgx@users.noreply.github.com>
2025-03-14 15:45:21 +0800

    界面

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,14 @@ mod resolution;
 
 #[tokio::main]
 async fn main() -> Result<(), eframe::Error> {
+    let size = egui::ViewportBuilder {
+        min_inner_size: Some(egui::Vec2::new(500.0, 400.0)),
+        max_inner_size: Some(egui::Vec2::new(500.0, 400.0)),
+        ..Default::default()
+    };
+
     let options = eframe::NativeOptions {
+        viewport: size,
         ..Default::default()
     };
 
@@ -32,6 +39,7 @@ impl MyApp {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         load_fonts(&_cc.egui_ctx);
         Self {
+            current_view: View::MainMenu,
             url: String::new(),
             selected_resolution: String::new(),
             resolutions: vec![
@@ -48,6 +56,7 @@ impl MyApp {
             pic: false,
             mutex_login: Arc::new(Mutex::new(false)),
             mutex_info: Arc::new(Mutex::new(String::new())),
+            save_path: "./download".to_string(),
         }
     }
     fn update_info(&mut self, info: String) {
@@ -86,7 +95,10 @@ impl MyApp {
         };
         println!("{:?}", video);
         let mutex_info = Arc::clone(&self.mutex_info);
-        let rsl = self.selected_resolution.clone();
+        let mut rsl = self.selected_resolution.clone();
+        if self.selected_resolution.len() == 0 {
+            rsl = "4K".to_string();
+        }
         tokio::spawn(async move {
             match init_::get_title_pic(&video).await {
                 Ok((t, _)) => {
@@ -109,8 +121,21 @@ impl MyApp {
     }
 }
 
+enum View {
+    MainMenu,
+    Settings,
+    About,
+}
+
+impl Default for View {
+    fn default() -> Self {
+        View::MainMenu
+    }
+}
+
 #[derive(Default)]
 struct MyApp {
+    current_view: View,
     url: String,
     selected_resolution: String,
     resolutions: Vec<String>,
@@ -118,126 +143,162 @@ struct MyApp {
     pic: bool,
     mutex_login: Arc<Mutex<bool>>,
     mutex_info: Arc<Mutex<String>>,
+    save_path: String,
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            let available_size = ui.available_size();
+        egui::CentralPanel::default().show(ctx, |ui| match self.current_view {
+            View::MainMenu => {
+                let available_size = ui.available_size();
+                // 动态计算各个部件的大小
+                let button_size = Vec2::new(available_size.x * 0.2, available_size.y * 0.05);
+                let text_edit_width = available_size.x * 0.8;
+                let text_edit_height = available_size.y * 0.1;
+                let image_size = Vec2::new(available_size.x * 0.2, available_size.x * 0.2);
+                let info_width = available_size.x * 0.6;
+                let info_height = available_size.y * 0.1;
 
-            // 动态计算各个部件的大小
-            let button_size = Vec2::new(available_size.x * 0.2, available_size.y * 0.05);
-            let text_edit_width = available_size.x * 0.8;
-            let text_edit_height = available_size.y * 0.1;
-            let image_size = Vec2::new(available_size.x * 0.2, available_size.x * 0.2);
-            let info_width = available_size.x * 0.6;
-            let info_height = available_size.y * 0.3;
-
-            ui.horizontal(|ui| {
-                ui.heading("bilibili视频下载器");
-                if ui
-                    .add_sized(button_size, egui::Button::new("登录"))
-                    .clicked()
-                {
-                    self.login();
-                }
-                if ui
-                    .add_sized(button_size, egui::Button::new("登出"))
-                    .clicked()
-                {
-                    println!("登出按钮点击");
-                    match std::fs::remove_file("load") {
-                        Ok(_) => println!("delogin successful"),
-                        Err(e) => eprintln!("Error occurred: {}", e),
+                ui.horizontal(|ui| {
+                    if ui.button("设置").clicked() {
+                        self.current_view = View::Settings;
                     }
-                }
-            });
-
-            ui.separator();
-
-            ui.horizontal(|ui| {
-                ui.label("视频地址输入:");
-                ui.add_sized(
-                    Vec2::new(text_edit_width, text_edit_height),
-                    egui::TextEdit::multiline(&mut self.url),
-                );
-            });
-
-            ui.horizontal(|ui| {
-                if ui
-                    .add_sized(button_size, egui::Button::new("下载"))
-                    .clicked()
-                {
-                    println!("分辨率:{}", self.selected_resolution);
-                    self.handle_down();
-                }
-            });
-
-            ui.horizontal(|ui| {
-                ui.label("分辨率");
-                egui::ComboBox::new(egui::Id::new("resolution_select"), "")
-                    .selected_text(&self.selected_resolution)
-                    .show_ui(ui, |ui| {
-                        for resolution in &self.resolutions {
-                            ui.selectable_value(
-                                &mut self.selected_resolution,
-                                resolution.clone(),
-                                resolution,
-                            );
-                        }
-                    });
-            });
-
-            ui.separator();
-
-            ui.horizontal(|ui| {
-                let info_text = {
-                    let info = self.mutex_info.clone();
-                    tokio::task::block_in_place(|| {
-                        let lock = futures::executor::block_on(info.lock());
-                        lock.clone()
-                    })
-                };
-                self.update_info(info_text.clone());
-                if self.info.is_empty() {
-                    self.update_pic(false);
-                } else {
-                    self.update_pic(true);
-                }
-                ui.vertical(|ui| {
-                    ui.label("封面:");
-                    //println!("pic:{}", self.pic);
-                    if self.pic {
-                        let path = Path::new("../pic.png");
-                        if path.exists() {
-                            ui.add(
-                                egui::Image::new(egui::include_image!("..\\pic.png"))
-                                    .max_width(200.0)
-                                    .rounding(10.0),
-                            );
-                        } else {
-                            ui.label("no download picture");
-                        }
-                    } else {
-                        ui.label("no picture");
+                    if ui.button("关于").clicked() {
+                        self.current_view = View::About;
                     }
                 });
-                ui.vertical(|ui| {
-                    ui.label("标题:");
+                ui.separator();
+
+                ui.horizontal(|ui| {
+                    ui.heading("bilibili视频下载器");
+                });
+
+                ui.horizontal(|ui| {
+                    if ui
+                        .add_sized(button_size, egui::Button::new("登录"))
+                        .clicked()
+                    {
+                        self.login();
+                    }
+                    if ui
+                        .add_sized(button_size, egui::Button::new("登出"))
+                        .clicked()
+                    {
+                        println!("登出按钮点击");
+                        match std::fs::remove_file("load") {
+                            Ok(_) => println!("delogin successful"),
+                            Err(e) => eprintln!("Error occurred: {}", e),
+                        }
+                    }
+                });
+
+                ui.separator();
+
+                ui.horizontal(|ui| {
+                    ui.label("视频地址输入:");
                     ui.add_sized(
-                        Vec2::new(info_width, info_height),
-                        egui::TextEdit::multiline(&mut self.info)
-                            .desired_rows(3)
-                            .desired_width(f32::INFINITY)
-                            .interactive(false),
+                        Vec2::new(text_edit_width, text_edit_height),
+                        egui::TextEdit::multiline(&mut self.url),
                     );
                 });
-            });
 
-            ui.separator();
+                ui.horizontal(|ui| {
+                    ui.label("分辨率");
+                    egui::ComboBox::new(egui::Id::new("resolution_select"), "")
+                        .selected_text(&self.selected_resolution)
+                        .show_ui(ui, |ui| {
+                            for resolution in &self.resolutions {
+                                ui.selectable_value(
+                                    &mut self.selected_resolution,
+                                    resolution.clone(),
+                                    resolution,
+                                );
+                            }
+                        });
+                    if ui
+                        .add_sized(button_size, egui::Button::new("下载"))
+                        .clicked()
+                    {
+                        println!("分辨率:{}", self.selected_resolution);
+                        self.handle_down();
+                    }
+                });
 
-            ui.label("process:");
-            // ui.add(ProgressBar::new(self.progress).show_percentage());
+                ui.separator();
+
+                ui.horizontal(|ui| {
+                    let info_text = {
+                        let info = self.mutex_info.clone();
+                        tokio::task::block_in_place(|| {
+                            let lock = futures::executor::block_on(info.lock());
+                            lock.clone()
+                        })
+                    };
+                    self.update_info(info_text.clone());
+                    if self.info.is_empty() {
+                        self.update_pic(false);
+                    } else {
+                        self.update_pic(true);
+                    }
+                    ui.vertical(|ui| {
+                        ui.label("封面:");
+                        //println!("pic:{}", self.pic);
+                        if self.pic {
+                            let path = Path::new("../pic.png");
+                            if path.exists() {
+                                // let path_str = path.to_str().unwrap().to_string();
+                                // ui.add(
+                                //     egui::Image::new(egui::include_image!("../pic.png"))
+                                //         .max_width(200.0)
+                                //         .rounding(10.0),
+                                // );
+                            } else {
+                                ui.label("no download picture");
+                            }
+                        } else {
+                            ui.label("no picture");
+                        }
+                    });
+                    ui.vertical(|ui| {
+                        ui.label("标题:");
+                        ui.add_sized(
+                            Vec2::new(info_width, info_height),
+                            egui::TextEdit::multiline(&mut self.info)
+                                .desired_rows(3)
+                                .desired_width(f32::INFINITY)
+                                .interactive(false),
+                        );
+                    });
+                });
+
+                ui.separator();
+
+                ui.label("process:");
+                //ui.add(ProgressBar::new(self.progress).show_percentage());
+            }
+            View::Settings => {
+                let available_size = ui.available_size();
+                if ui.button("Go to Main Menu").clicked() {
+                    self.current_view = View::MainMenu;
+                }
+                ui.heading("Settings");
+
+                ui.horizontal(|ui| {
+                    ui.label("Save Path:");
+                    ui.add_sized(
+                        Vec2::new(available_size.x * 0.5, available_size.y * 0.01),
+                        egui::TextEdit::multiline(&mut self.save_path),
+                    );
+                });
+            }
+            View::About => {
+                if ui.button("Go to Main Menu").clicked() {
+                    self.current_view = View::MainMenu;
+                }
+                ui.heading("About");
+
+                ui.label("这里是关于页面");
+            }
         });
     }
 }
@@ -269,57 +330,3 @@ async fn login() {
         println!("Login failed");
     }
 }
-
-// async fn initiation() {
-//     login().await;
-//     loop {
-//         let mut url = String::new();
-//         println!("\nPlease input the url of the bilibili to download, or 'exit' to exit:");
-//         loop {
-//             url.clear();
-//             io::stdin()
-//                 .read_line(&mut url)
-//                 .expect("Failed to read line");
-//             if url != "\r\n" {
-//                 break;
-//             }
-//         }
-
-//         if url.trim() == "exit" {
-//             break;
-//         }
-
-//         let video = match init_::get_epid_season(&url) {
-//             Ok(v) => v,
-//             Err(e) => {
-//                 eprintln!("Error occurred: {}", e);
-//                 continue;
-//             }
-//         };
-//         println!("{:?}", video);
-//         if let Err(e) = init_::choose_download_method(&video).await {
-//             eprintln!("Error occurred: {}", e);
-//         }
-//     }
-// }
-
-// /// 调用二维码登录函数
-// async fn login_() {
-//     println!("waiting for login...");
-//     let client: Client = reqwest::Client::new();
-//     match refresh_cookie::refresh_cookie(&client).await {
-//         Ok(flag) => {
-//             if flag {
-//                 println!("dont need to refresh cookie\nLogin successful");
-//             } else {
-//                 println!("cookie is out of date or havent logined, please login again");
-//                 if login_qrcode(&client).await {
-//                     println!("Login successful");
-//                 } else {
-//                     println!("Login failed");
-//                 }
-//             }
-//         }
-//         Err(e) => eprintln!("Error occurred: {}", e),
-//     }
-// }
